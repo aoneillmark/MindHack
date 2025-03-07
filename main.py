be256c3632fc25c6a015b36d2da5d29094204e6d
Lauren Robson <lar19@ic.ac.uk>
2025-03-07 12:57:31 +0000

    added method to generate example child questions and responses

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from google import genai
 import os
-
-
-def get_reply(prompt, model="gemini-2.0-flash", verbose=False):
-    response = client.models.generate_content(model=model, contents=prompt)
-    if verbose:
-        print(response.text)
-    return response.text
+from utils import get_reply
 
 
 if __name__ == "__main__":
@@ -18,21 +12,29 @@ if __name__ == "__main__":
     # initialise client
     client = genai.Client(api_key=key)
 
-    # get prompt from text file
-    prompt_file = "prompts/generate_child_prompts.txt"
+    # get child prompts from text file
+    prompt_file = "prompts/generated_child_prompts.txt"
     with open(prompt_file) as f:
-        prompt = f.readlines()
-    prompt = "\n".join(prompt)
+        prompts = f.readlines()
+    
+    # remove any empty lines
+    prompts = [prompt.strip() for prompt in prompts if prompt.strip()]
+    
+    # generate content for each prompt
+    replies = []
+    for prompt in prompts:
+        replies.append(get_reply(prompt, client=client))
 
-    # generate content
-    reply = get_reply(prompt)
-
-    # save the reply to a text file for later use:
-    save_file = "prompts/generated_child_prompts.txt"
+    # save the replies to a text file for later use:
+    save_file = "replies/generated_child_replies.txt"
     # check if file exists, if it does not exist, create it
     if not os.path.exists(save_file):
         os.makedirs(os.path.dirname(save_file), exist_ok=True)
 
-    # append the reply to the file
-    with open(save_file, "a") as f:
-        f.write(reply + "\n")
+    # append the replies to the file
+    with open(save_file, "a", encoding="utf-8") as f:
+        for prompt, reply in zip(prompts, replies):
+            f.write("Prompt:\n")
+            f.write(prompt + "\n")
+            f.write("Reply:\n")
+            f.write(reply + "\n")
--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def get_reply(prompt, model="gemini-2.0-flash", verbose=False, client=None):
     # create the logs directory if it does not exist
     if not os.path.exists("logs"):
         os.makedirs("logs")
-    with open(filename, "w") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         f.write("Prompt:\n")
         f.write(prompt + "\n")
         f.write("Response:\n")
